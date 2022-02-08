94929ab5ce2500b71f4af68a5b50e7c0018d0848
Pat Barfield <patrickhbarfield@gmail.com>
2022-02-08 15:16:26 -0600

    add the second test solution for intro-to-testing

--- tests.js
+++ tests.js
@@ -1,7 +1,7 @@
 // Unit tests for the helloWorld function
 describe('sayHello', function() {
-    it('should be a defined function', function() {
-      expect(typeof sayHello).toBe('function');
+    it('should return a string when called', function() {
+      expect(typeof sayHello()).toBe('string');
     });
     it('should return a string when called', function() {
       expect(typeof sayHello()).toBe("string");
